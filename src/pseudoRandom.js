725195ec2d6d8c17d075f7c2f1636c51acdf13b9
Peeke <pkuepers@kaliber.net>
2022-05-06 11:33:06 +0200

    Update src/pseudoRandom.js
    Co-authored-by: EECOLOR <eecolor@qirx.org>

--- src/pseudoRandom.js
+++ src/pseudoRandom.js
@@ -6,7 +6,8 @@ export function pseudoRandom(seed) {
 
 // Seed generator: https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript/47593316#47593316
 export function xmur3(str) {
-  for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
+  let h = 1779033703 ^ str.length
+  for(let i = 0, i < str.length; i++) {
       h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
       h = h << 13 | h >>> 19;
   } 
