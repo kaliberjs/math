b29a9588749f3a0423f792e4b3455d88dbc57244
Peeke Kuepers <pkuepers@kaliber.net>
2022-05-09 10:44:57 +0200

    Split into randomNumberGenerator and pseudoRandom

---
+++ src/randomNumberGenerator.js
@@ -0,0 +1,40 @@
+/**
+ * Return pseudo random number generator. Given the same seed, this function will always 
+ * return the same sequence of random numbers.
+ * @param {string|number} seed - The seed the number generator is based on
+ * @returns {number} - A number between 0 (inclusive) and 4294967296 (exclusive)
+ */
+export function randomNumberGenerator(seed) {
+  return xmur3(String(seed))
+}
+
+/**
+ * Return a pseudo random number. Given the same seed, this function will always 
+ * return the same random number.
+ * @param {string|number} seed - The seed used to determine the random number
+ * @returns {number} - A number between 0 (inclusive) and 1 (exclusive)
+ */
+export function pseudoRandom(seed) {
+  const random = randomNumberGenerator(seed)
+  return random() / 4294967296
+}
+
+// https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript/47593316#47593316
+// Hash function that generates high entropy 32 bit number (unsigned)
+function xmur3(str) {
+  let i = 0
+  let h = 1779033703 ^ str.length
+
+  for (; i < str.length; i++) {
+    h = Math.imul(h ^ str.charCodeAt(i), 3432918353)
+    h = h << 13 | h >>> 19
+  }
+
+  return function () {
+    h = Math.imul(h ^ (h >>> 16), 2246822507)
+    h = Math.imul(h ^ (h >>> 13), 3266489909)
+    h ^= h >>> 16
+
+    return h >>> 0
+  }
+}
